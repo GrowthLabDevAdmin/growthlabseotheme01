8d861ce237274d6149a5b9e87901dd301990502a
ArturoValv <art.valverde.563@gmail.com>
2026-03-09 23:26:17 -0600

    fix: Fixed many things on acf ci/cd scripts to prevent subfields  deletions

--- theme-functions/acf-functions.php
+++ theme-functions/acf-functions.php
@@ -185,10 +185,8 @@ function my_acf_json_load_point($paths)
     return $paths;
 }
 
-add_action('init', function () {
-    add_filter('acf/settings/save_json', 'my_acf_json_save_point');
-    add_filter('acf/settings/load_json', 'my_acf_json_load_point');
-});
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
 
 
 /**
@@ -200,7 +198,6 @@ add_action('init', function () {
  * - El hash MD5 de los archivos JSON para detectar cambios
  */
 add_action('admin_notices', function () {
-    return;
     if (wp_doing_ajax() || wp_doing_cron()) return;
     if (!current_user_can('manage_options')) return;
     if (defined('ACF_DOING_SYNC')) return;
